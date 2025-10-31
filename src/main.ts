1ca9c93dea579af70bada302a8de13cc6a480425
Jacobo Ossa <jaco2419@gmail.com>
2025-10-30 22:59:32 -0500

    Fix warning for listen port

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   logger.log(`🚀 Application is running on: http://localhost:${port}`);
   logger.log(
     `🔌 WebSocket server is running on: ws://localhost:${port}/payments`,
