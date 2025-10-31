ff8a421442aa459380f781cc2d5dfae28ad3be94
Jacobo Ossa <jaco2419@gmail.com>
2025-10-31 14:48:37 -0500

    Fix webhook DTO

--- src/webhook/dto/wompi-webhook.dto.ts
+++ src/webhook/dto/wompi-webhook.dto.ts
@@ -1,80 +1,26 @@
-import {
-  IsString,
-  IsNotEmpty,
-  IsNumber,
-  ValidateNested,
-} from "class-validator";
-import { Type } from "class-transformer";
-
-class TransactionDto {
-  @IsString()
-  @IsNotEmpty()
-  id: string;
-
-  @IsNumber()
-  amount_in_cents: number;
-
-  @IsString()
-  @IsNotEmpty()
-  reference: string;
-
-  @IsString()
-  customer_email: string;
-
-  @IsString()
-  currency: string;
-
-  @IsString()
-  payment_method_type: string;
-
-  @IsString()
-  redirect_url: string;
-
-  @IsString()
-  @IsNotEmpty()
-  status: string;
-
-  shipping_address: any;
-
-  @IsString()
-  payment_link_id: string;
-
-  payment_source_id: any;
-}
-
-class SignatureDto {
-  @IsString()
-  @IsNotEmpty()
-  checksum: string;
-
-  properties: string[];
-}
-
-class WebhookDataDto {
-  @ValidateNested()
-  @Type(() => TransactionDto)
-  transaction: TransactionDto;
-}
-
 export class WompiWebhookDto {
-  @IsString()
-  @IsNotEmpty()
   event: string;
-
-  @ValidateNested()
-  @Type(() => WebhookDataDto)
-  data: WebhookDataDto;
-
-  @IsString()
+  data: {
+    transaction: {
+      id: string;
+      amount_in_cents: number;
+      reference: string;
+      customer_email: string;
+      currency: string;
+      payment_method_type: string;
+      redirect_url: string;
+      status: string;
+      shipping_address: any;
+      payment_link_id: string;
+      payment_source_id: any;
+    };
+  };
   environment: string;
 
-  @ValidateNested()
-  @Type(() => SignatureDto)
-  signature: SignatureDto;
-
-  @IsNumber()
+  signature: {
+    checksum: string;
+    properties: string[];
+  };
   timestamp: number;
-
-  @IsString()
   sent_at: string;
 }
